1e28dfefa9248e50ef418904d9fe6c3fd63fdab6
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-11-07 11:58:36 +0100

    Opdracht 6

--- bestemmingen.php
+++ bestemmingen.php
@@ -2,50 +2,66 @@
 require_once('databaseConn.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link href="https://fonts.googleapis.com/css?family=Orbitron|Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-    <meta charset="UTF-8">
-    <title>Dehortop Trips and Travels</title>
-</head>
-<body>
-<div id="wrapper">
-    <header>
-        <h4>Dehortop Reizen</h4>
-        <nav>
-            <a href="index.html">Home</a> |
-            <a href="bestemmingen.php">Bestemmingen</a> |
-            <a href="">Reizen</a> |
-            <a href="">Registreer</a> |
-            <a href="">Inloggen</a>
-        </nav>
-    </header>
-    <div id="overzichtHouder">
-        <h3>KKAlternative</h3>
-        <?php
-        //Display the data in a table
-        $test = $conn->query("SELECT Bestemmingcode, Plaats, Land, Werelddeel FROM bestemmingen");
-        print "<table class ='overzicht'>";
-        print "<tr><th>Code</th><th>Plaats</th><th>Land</th><th>Werelddeel</th></tr>";
-        foreach ($test as $row) {
-            print "<tr>";
-            print "<td>" . $row['Bestemmingcode'] . "</td>";
-            print "<td>" . $row['Plaats'] . "</td>";
-            print "<td>" . $row['Land'] . "</td>";
-            print "<td>" . $row['Werelddeel'] . "</td>";
-            print "</tr>";
-        }
-        print "</table>";
-        ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Orbitron|Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <meta charset="UTF-8">
+        <title>Dehortop Trips and Travels</title>
+    </head>
+    <body>
+    <div id="wrapper">
+        <header>
+            <h4>Dehortop Reizen</h4>
+            <nav>
+                <a href="index.html">Home</a> |
+                <a href="bestemmingen.php">Bestemmingen</a> |
+                <a href="">Reizen</a> |
+                <a href="">Registreer</a> |
+                <a href="">Inloggen</a>
+            </nav>
+        </header>
+        <form action="bestemmingen.php" method="get" enctype="multipart/form-data">
+            <div id="overzichtHouder">
+                <?php
+
+                //Display the data in a table
+                $orderBy = array('Bestemmingcode', 'Plaats', 'Land', 'Werelddeel');
+
+                $order = 'Bestemmingcode';
+                if (isset($_GET['sort']) && in_array($_GET['sort'], $orderBy)) {
+                    $order = $_GET['sort'];
+                }
+
+                $test = $conn->query('SELECT * FROM bestemmingen ORDER BY ' . $order);
+
+                print "<table class ='overzicht'>";
+                print "<tr>
+                       <th><a href=\"bestemmingen.php?sort=Bestemmingcode\" id='code'>Code</a></th>
+                       <th><a href=\"bestemmingen.php?sort=Plaats\" id='plaats'>Plaats</a></th>
+                       <th><a href=\"bestemmingen.php?sort=Land\" id='land'>Land</a></th>
+                       <th><a href=\"bestemmingen.php?sort=Werelddeel\" id='werelddeel'>Werelddeel</a></th>
+                       </tr>";
+
+                foreach ($test as $row) {
+                    print "<tr>";
+                    print "<td>" . $row['Bestemmingcode'] . "</td>";
+                    print "<td>" . $row['Plaats'] . "</td>";
+                    print "<td>" . $row['Land'] . "</td>";
+                    print "<td>" . $row['Werelddeel'] . "</td>";
+                    print "</tr>";
+                }
+                print "</table>";
+                ?>
+            </div>
+        </form>
+        <footer>
+            <p>&copy; Dehortop 2019</p>
+        </footer>
     </div>
-    <footer>
-        <p>&copy; Dehortop 2019</p>
-    </footer>
-</div>
-</body>
-</html>
+    </body>
+    </html>
 
 <?php
 
--- databaseConn.php
+++ databaseConn.php
@@ -8,7 +8,6 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=2119173_namara_kerkenaar;port=3306", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
